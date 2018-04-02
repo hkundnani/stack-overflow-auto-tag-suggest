cd19b1e67a2dc3c1d7938633e7a8bbd62b1fbf1f
ankitajaiwar <32300972+ankitajaiwar@users.noreply.github.com>
2018-04-02 16:37:49 -0400

    Connection to postgresql database through python

---
+++ connection.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 02 15:53:45 2018
+
+@author: Ankita
+"""
+
+#Make connection with postgesql
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='stackoverflow' user='Ankita' host='localhost' password='aj1312AJ'")
+    cur = conn.cursor()
+    
+    print("Connection created")
+    cur.execute("select id from posts limit 5")
+    print("selected ids")
+    print(cur.rowcount)
+    row = cur.fetchone()
+    while row is not None:
+        print(row)
+        row = cur.fetchone()
+    cur.close()
+    conn.close()
+except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
