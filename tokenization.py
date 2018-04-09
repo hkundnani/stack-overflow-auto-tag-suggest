bf246963212916cd60b77c62297092a6f69ffad2
harshk <hkundnani@gmail.com>
2018-04-09 18:55:28 -0400

    Update code to store strings in tags rather than list

--- tokenization.py
+++ tokenization.py
@@ -25,13 +25,13 @@ def process(chunk):
         return " ".join(processed_words)
     def tokenizer(x):
         source = unicode(x, 'utf-8')
-        return word_tokenize(source)
+        tokens = word_tokenize(source)
+        return ','.join([token for token in tokens if token != '.'])
     def tag_process(x):
-        regex = r"<|>"
-        return re.sub(regex, " ", x)
-
-        
-        
+        # regex = r"<|>"
+        # return re.sub(regex, " ", x)
+        return x.replace('<', '')
+           
     chunk["title"] = chunk["title"].map(rem_punct_url)
    
     chunk["title"] = chunk["title"].map(rem_stop_words)
@@ -41,18 +41,16 @@ def process(chunk):
     chunk["title"] = chunk["title"].map(tokenizer)
     chunk["body"] = chunk["body"].map(tokenizer)
     chunk["tags"] = chunk["tags"].map(tag_process)
-    chunk["tags"] = chunk["tags"].map(tokenizer)
-    print (chunk.head())
+    chunk["tags"] = chunk["tags"].map(lambda tag: ','.join(filter(None, unicode(tag).split('>'))))
+    # print (chunk.head())
        # chunk["title"]=re.sub(uri_re, "", chunk["title"])
         #chunk["body"]=re.sub(uri_re, "", chunk["body"])
     #print(chunk.head())   
     
-i =0
+i = 0
 chunksize = 100000
-for chunk in pd.read_csv("E:/required_data.csv", chunksize=chunksize):
+for chunk in pd.read_csv("stack-overflow.csv", chunksize=chunksize):
     process(chunk)
-    chunk.to_csv("E:/processed_data.csv", encoding='utf-8', mode = 'a', index=False)
+    chunk.to_csv("processed_data.csv", encoding='utf-8', mode = 'a', index=False)
     print("Chunk number"+str(i))
     i=i+1
-    
-    
